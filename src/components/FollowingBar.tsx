61e14fcaa674516c358701ffbff127485094098f
Henry Chun <chunhyungjoon@gmail.com>
2023-07-12 08:44:24 +1000

    done followingBar

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,8 +1,37 @@
+'use client';
+import { DetailUser } from '@/models/user';
+import { PropagateLoader } from 'react-spinners';
 import useSWR from 'swr';
+import ScrollableBar from './ui/ScrollBar';
+import Link from 'next/link';
+import Avatar from './Avatar';
 
 export default function FollowingBar() {
-  const { data, isLoading, error } = useSWR('/api/hello');
-  console.log(data);
-
-  return <div>FollowingBar</div>;
+  const { data, isLoading: loading, error } = useSWR<DetailUser>('/api/me');
+  const users = data?.following;
+  return (
+    <section className='w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto relative z-0'>
+      {loading ? (
+        <PropagateLoader size={8} color='red' />
+      ) : (
+        (!users || users.length === 0) && <p>Not following anyone</p>
+      )}
+      {users && users.length > 0 && (
+        <ScrollableBar>
+          {users.map(({ image, username }) => (
+            <Link
+              key={username}
+              className='flex flex-col items-center w-20'
+              href={`/user/${username}`}
+            >
+              <Avatar image={image} highlight />
+              <p className='w-full overflow-hidden text-sm text-center text-ellipsis'>
+                {username}
+              </p>
+            </Link>
+          ))}
+        </ScrollableBar>
+      )}
+    </section>
+  );
 }
