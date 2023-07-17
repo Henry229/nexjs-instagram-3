e291664ff70a978ad03fb46b48447decdfab1596
henry chun <henry.chun@cseducation.com.au>
2023-07-18 09:15:31 +1000

    adding likes feature but still got error

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -1,25 +1,52 @@
 import HeartIcon from './ui/icons/HeartIcon';
 import BookmarkIcon from './ui/icons/BookmarkIcon';
 import { parseDate } from '@/util/date';
+import { useState } from 'react';
+import HeartFillIcon from './ui/icons/HeartFillIcon';
+import ToggleButton from './ui/ToggleButton';
+import BookmarkFillIcon from './ui/icons/BookmarkFillIcon';
+import { SimplePost } from '@/models/post';
+import { useSession } from 'next-auth/react';
+import { useSWRConfig } from 'swr';
 
 type Props = {
-  likes: string[];
-  username: string;
-  text?: string;
-  createdAt: string;
+  post: SimplePost;
 };
 
-export default function ActionBar({ likes, username, text, createdAt }: Props) {
+export default function ActionBar({ post }: Props) {
+  const { id, likes, username, text, createdAt } = post;
+  const { data: session } = useSession();
+  const user = session?.user;
+  const liked = user ? likes.includes(user.username) : false;
+  const [bookmarked, setBookmarked] = useState(false);
+  const { mutate } = useSWRConfig();
+  const handleLike = (like: boolean) => {
+    fetch('/api/likes', {
+      method: 'PUT',
+      body: JSON.stringify({ id, like }),
+    }).then(() => mutate('api/posts'));
+    // like를 업데이트 했으면 SWR에게 전체 cache를 update해달라고 하는 것
+  };
   return (
     <>
-      <div className='flex justify-between px-4 m-2'>
-        <HeartIcon />
-        <BookmarkIcon />
+      <div className='flex justify-between px-4 my-2'>
+        <ToggleButton
+          toggled={liked}
+          onToggle={handleLike}
+          onIcon={<HeartFillIcon />}
+          offIcon={<HeartIcon />}
+        />
+        <ToggleButton
+          toggled={bookmarked}
+          onToggle={setBookmarked}
+          onIcon={<BookmarkFillIcon />}
+          offIcon={<BookmarkIcon />}
+        />
       </div>
       <div className='px-4 py-1'>
         {/* likes?.length ?? 0 likes가 있고 length가 있으면 쓰고 없으면 0 */}
         <p className='mb-2 text-sm font-bold'>
-          {`${likes?.length ?? 0} ${likes?.length > 1 ? 'likes' : 'like'}`}{' '}
+          {`${likes?.length ?? 0} ${likes?.length > 1 ? 'likes' : 'like'}`}
         </p>
         {text && (
           <p>
--- src/services/posts.ts
+++ src/services/posts.ts
@@ -1,3 +1,4 @@
+import post from '../../sanit-studio/schemas/post';
 import { SimplePost } from '@/models/post';
 import { client, urlFor } from './sanity';
 
@@ -22,13 +23,11 @@ export async function getFollowingPostsOf(username: string) {
   //2번째 쿼리는 조인쿼리로 user와 username이 같은 걸 조인해서 following 배열의 username을 가져온다
   return client
     .fetch(
-      `*[_type == "post" && author->username == "${username}"
+      `*[_type =="post" && author->username == "${username}"
     || author._ref in *[_type == "user" && username == "${username}"].following[]._ref] 
     | order(_createdAt desc){${simplePostProjection}}`
     )
-    .then((posts) =>
-      posts.map((post: SimplePost) => ({ ...post, image: urlFor(post.image) }))
-    );
+    .then(mapPosts);
 }
 
 export async function getPost(id: string) {
@@ -45,7 +44,7 @@ export async function getPost(id: string) {
         "createdAt":_createdAt
       }`
     )
-    .then(mapPosts);
+    .then((post) => ({ ...post, image: urlFor(post.image) }));
 }
 
 export async function getPostsOf(username: string) {
@@ -78,4 +77,26 @@ function mapPosts(posts: SimplePost[]) {
     likes: post.likes ?? [],
     image: urlFor(post.image),
   }));
+}
+
+export async function likePost(postId: string, userId: string) {
+  console.log('@@@ postId', postId);
+
+  return client
+    .patch(postId)
+    .setIfMissing({ likes: [] })
+    .append('likes', [
+      {
+        _ref: userId,
+        _type: 'reference',
+      },
+    ])
+    .commit({ autoGenerateArrayKeys: true }); // id를 자동으로 생성하게 함
+}
+
+export async function dislikePost(postId: string, userId: string) {
+  return client
+    .patch(postId)
+    .unset([`likes[_ref=="${userId}"]`])
+    .commit();
 }
