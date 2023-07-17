66e089b285ba8f04fe29880e5c8070755b4dd95d
henry chun <henry.chun@cseducation.com.au>
2023-07-17 19:59:10 +1000

    ssg-> ssr

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,6 +1,8 @@
 import { searchUsers } from '@/services/user';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   return searchUsers().then((data) => NextResponse.json(data));
 }
